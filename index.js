a9832890116943a0ef9b7e51d739a894c2f56a2b
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 14:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ get area(){
 get circumference(){
     return this.radius *2 * Math.PI;
   }
+  set diameter(diameter){
+    this.radius = diameter / 2
+  }
+  
+   set circumference(circumference){
+    this.radius = circumference / ( Math.PI * 2 )
+  }
+  
+  
+  set area(area){
+    this.radius = Math.sqrt( area / Math.PI )
+  }
 }
