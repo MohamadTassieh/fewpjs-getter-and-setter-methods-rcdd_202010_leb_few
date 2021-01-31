82418c7857a90531653b16f9c7ac59241099fb1f
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 14:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ class Circle {
   constructor(radius){
     this.radius = radius;
   }
+get diameter(){
+  return this.raddius * 2;
 }
+get area(){
+  return Math.PI * this.radius * this.radius;
+}
+get circumference(){
+    return this.radius *2 * Math.PI;
+  }
+}
+
+
